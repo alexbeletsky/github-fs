c823087c215e88cbb514d8d4c03669ac485bd0d2
alexander.beletsky <alexander.beletsky@gmail.com>
2012-10-09 22:12:25 +0300

    pair programming with @vansha

---
+++ src/Github.FileSystem.Tests/Integration/FileSystemTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Github.Tests.Integration
+{
+    public class FileSystemTests
+    {
+        [Test]
+        public void should_return_true_if_file_exist()
+        {
+            var fileSystem = FileSystem.Open("alexanderbeletsky", "github-fs.net");
+
+            Assert.That(fileSystem.FileExists("README.md"), Is.True);
+        }
+
+        [Test]
+        public void should_return_false_if_file_does_not_exist()
+        {
+            var fileSystem = FileSystem.Open("alexanderbeletsky", "github-fs.net");
+
+            Assert.That(fileSystem.FileExists("README_NotExist.md"), Is.False);
+        }
+
+        [Test]
+        public void should_return_false_if_directory_name_passed_instead_of_filename()
+        {
+            var fileSystem = FileSystem.Open("alexanderbeletsky", "candidate");
+
+            Assert.That(fileSystem.FileExists("src"), Is.False);
+        }
+
+        [Test]
+        public void should_return_true_if_directory_exists()
+        {
+            var fileSystem = FileSystem.Open("alexanderbeletsky", "candidate");
+
+            Assert.That(fileSystem.DirectoryExists("src"), Is.True);
+        }
+
+        [Test]
+        public void should_return_false_if_directory_not_exist()
+        {
+            var fileSystem = FileSystem.Open("alexanderbeletsky", "candidate");
+
+            Assert.That(fileSystem.DirectoryExists("src_not_exist"), Is.False);
+        }
+
+        [Test]
+        public void should_return_false_if_filename_passed_instead_directory_name()
+        {
+            var fileSystem = FileSystem.Open("alexanderbeletsky", "candidate");
+
+            Assert.That(fileSystem.DirectoryExists("README.md"), Is.False);
+        }
+
+    }
+}
---
+++ src/Github.FileSystem/FileSystem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Github
+{
+    public class FileSystem : IFileSystem
+    {
+        private readonly string _owner;
+        private readonly string _repository;
+        private const string ApiBaseUrl = "https://api.github.com";
+
+        private FileSystem(string owner, string repository)
+        {
+            _owner = owner;
+            _repository = repository;
+        }
+
+        public static IFileSystem Open(string owner, string repository)
+        {
+            return new FileSystem(owner, repository);
+        }
+
+        public bool FileExists(string filename)
+        {
+            var url = string.Format("{0}/repos/{1}/{2}/contents/{3}", ApiBaseUrl, _owner, _repository, filename);
+            var httpClient = new HttpClient();
+
+            var result = httpClient.GetAsync(url).Result;
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var response = JsonConvert.DeserializeObject<Contents>(result.Content.ReadAsStringAsync().Result);
+                    return response.Type == "file";
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public bool DirectoryExists(string directory)
+        {
+            var url = string.Format("{0}/repos/{1}/{2}/contents/{3}", ApiBaseUrl, _owner, _repository, directory);
+            var httpClient = new HttpClient();
+
+            var result = httpClient.GetAsync(url).Result;
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var value = result.Content.ReadAsStringAsync().Result;
+                try
+                {
+                    var response = JsonConvert.DeserializeObject<IList<Contents>>(value);
+                    return response.Any();
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class Contents
+    {
+        public string Type { get; set; }
+
+        public string Encoding { get; set; }
+
+        public int Size { get; set; }
+    }
+
+    public class Credentials
+    {
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+
+    public interface IFileSystem
+    {
+        bool FileExists(string filename);
+        bool DirectoryExists(string directory);
+    }
+}
